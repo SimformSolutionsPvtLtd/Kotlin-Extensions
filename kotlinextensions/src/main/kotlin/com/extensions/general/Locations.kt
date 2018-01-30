eb6be792b3e57730f97846d6312ab67c282a677b
Krunal Kevadiya <krunal.k@simformsolutions.com>
2018-01-30 09:58:46 +0530

    Feat : Complete to change location service.

--- kotlinextensions/src/main/kotlin/com/extensions/general/Locations.kt
+++ kotlinextensions/src/main/kotlin/com/extensions/general/Locations.kt
@@ -12,107 +12,145 @@ import android.location.Address
 import android.location.Geocoder
 import android.location.Location
 import android.location.LocationManager
+import android.os.AsyncTask
 import android.os.Binder
 import android.os.Bundle
+import android.os.Looper
 import android.view.Surface
 import com.extensions.content.locationManager
 import com.extensions.content.sensorManager
 import com.extensions.content.windowManager
-import com.extensions.interfaces.F1
-import com.extensions.interfaces.F2
 import com.extensions.logging.Logger
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationListener
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
 import java.io.IOException
 import java.util.*
 
 @SuppressLint("MissingPermission", "Registered")
 class Locations : Service() {
+    private val localBinder = LocalBinder()
+    private val sensorEventListener = SensorListener()
+    private val fusedLocationListener = MyLocationListener()
     private val gpsLocationListener = LocationChangeListener()
     private val networkLocationListener = LocationChangeListener()
-    private val sensorEventListener = SensorListener()
-    private val localBinder = LocalBinder()
+    var mGoogleApiClient :GoogleApiClient? = null
 
     companion object {
+        val ARG_FUSED_LOCATION = "is_fused_location_api"
         private val TWO_MINUTES = 1000 * 60 * 2
         private val MIN_BEARING_DIFF = 2.0f
         private val FASTEST_INTERVAL_IN_MS = 1000L
     }
 
-    private var bearing: Float = 0f
     private var axisX: Int = 0
     private var axisY: Int = 0
-    var currentBestLocation: Location? = null
+    private var bearing: Float = 0f
+    private var isFusedLocationApi: Boolean = false
+    private var currentBestLocation: Location? = null
     private var locationCallback: LocationCallback? = null
 
     override fun onBind(intent: Intent?) = localBinder
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         super.onStartCommand(intent, flags, startId)
+        if(intent != null && intent.hasExtra(ARG_FUSED_LOCATION))
+            isFusedLocationApi = intent.getBooleanExtra(ARG_FUSED_LOCATION, false)
         return START_STICKY
     }
 
     override fun onCreate() {
         super.onCreate()
-        getLocation()
+        if(checkIfLocationIsEnabled())
+            getLocation()
+        else
+            locationCallback?.locationDisabled()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        stopUpdates()
+        if(isFusedLocationApi)
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, fusedLocationListener)
+        else
+            stopUpdates()
         sensorManager.unregisterListener(sensorEventListener)
     }
 
     private fun getLocation() {
-        val lastKnownGpsLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-        val lastKnownNetworkLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
-        var bestLastKnownLocation = currentBestLocation
+        if(isFusedLocationApi) {
+            if(mGoogleApiClient == null) {
+                mGoogleApiClient = GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(object :GoogleApiClient.ConnectionCallbacks {
+                        override fun onConnected(bundle :Bundle?) {
+                            getContinuousLocation()
+                        }
 
-        if (lastKnownGpsLocation != null && isBetterLocation(lastKnownGpsLocation, bestLastKnownLocation)) {
-            bestLastKnownLocation = lastKnownGpsLocation
+                        override fun onConnectionSuspended(i :Int) {}
+                    })
+                    .addOnConnectionFailedListener({connectionResult ->
+                        locationCallback?.onFailure(Exception(connectionResult.errorMessage))
+
+                        if(mGoogleApiClient != null) {
+                            if(mGoogleApiClient!!.isConnected)
+                                mGoogleApiClient!!.disconnect()
+                            mGoogleApiClient = null
+                        }
+                    })
+                    .addApi(LocationServices.API)
+                    .build()
+            }
+            if(mGoogleApiClient != null && !mGoogleApiClient!!.isConnected)
+                mGoogleApiClient!!.connect()
+        } else {
+            val lastKnownGpsLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            val lastKnownNetworkLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+            var bestLastKnownLocation = currentBestLocation
+
+            if(lastKnownGpsLocation != null && isBetterLocation(lastKnownGpsLocation, bestLastKnownLocation)) {
+                bestLastKnownLocation = lastKnownGpsLocation
+            }
+
+            if(lastKnownNetworkLocation != null && isBetterLocation(lastKnownNetworkLocation, bestLastKnownLocation)) {
+                bestLastKnownLocation = lastKnownNetworkLocation
+            }
+
+            currentBestLocation = bestLastKnownLocation
+
+            if(locationManager.allProviders.contains(LocationManager.GPS_PROVIDER) && locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, FASTEST_INTERVAL_IN_MS, 0.0f, gpsLocationListener)
+            }
+
+            if(locationManager.allProviders.contains(LocationManager.NETWORK_PROVIDER) && locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, FASTEST_INTERVAL_IN_MS, 0.0f, networkLocationListener)
+            }
+
+            bestLastKnownLocation?.bearing = bearing
+            locationCallback?.onSuccess(currentBestLocation as Location)
         }
-
-        if (lastKnownNetworkLocation != null && isBetterLocation(lastKnownNetworkLocation, bestLastKnownLocation)) {
-            bestLastKnownLocation = lastKnownNetworkLocation
-        }
-
-        currentBestLocation = bestLastKnownLocation
-
-        if (locationManager.allProviders.contains(LocationManager.GPS_PROVIDER) && locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, FASTEST_INTERVAL_IN_MS, 0.0f, gpsLocationListener)
-        }
-
-        if (locationManager.allProviders.contains(LocationManager.NETWORK_PROVIDER) && locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, FASTEST_INTERVAL_IN_MS, 0.0f, networkLocationListener)
-        }
-
-        bestLastKnownLocation?.bearing = bearing
-        locationCallback?.handleNewLocation(currentBestLocation as Location)
 
         val mSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
         sensorManager.registerListener(sensorEventListener, mSensor, SensorManager.SENSOR_DELAY_NORMAL * 5)
     }
 
-    fun setLocationCallback(callback: (Location) -> Unit) {
-        locationCallback = object : LocationCallback, (Location) -> Unit {
-            override fun invoke(location: Location) {
-                callback.invoke(location)
+    private fun getContinuousLocation() {
+        if(checkIfLocationIsEnabled()) {
+            class BackgroundTask : AsyncTask<Void, Void, Void>() {
+                override fun doInBackground(vararg params :Void?) :Void? {
+                    val locationRequest = LocationRequest()
+                    locationRequest.interval = FASTEST_INTERVAL_IN_MS
+                    locationRequest.fastestInterval = FASTEST_INTERVAL_IN_MS
+                    LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, locationRequest, fusedLocationListener, Looper.getMainLooper())
+                    return null
+                }
             }
-
-            override fun handleNewLocation(location: Location) {
-                callback.invoke(location)
-            }
-        }
+            BackgroundTask().execute()
+        } else
+            locationCallback?.locationDisabled()
     }
 
-    fun setLocationCallback(callback:F1<Location>) {
-        locationCallback = object : LocationCallback, (Location) -> Unit {
-            override fun invoke(location: Location) {
-                callback.invoke(location)
-            }
-
-            override fun handleNewLocation(location: Location) {
-                callback.invoke(location)
-            }
-        }
+    fun setLocationCallback(callback: LocationCallback) {
+        locationCallback = callback
     }
 
     fun stopUpdates() {
@@ -121,63 +159,32 @@ class Locations : Service() {
         sensorManager.unregisterListener(sensorEventListener)
     }
 
-    private fun Context.getGeoCoderAddress(): List<Address>? {
-        val geoCoder = Geocoder(this, Locale.ENGLISH)
-        try {
-            return geoCoder.getFromLocation(currentBestLocation?.latitude ?: 0.0, currentBestLocation?.longitude ?: 0.0, 1)
-        } catch (e: IOException) {
-            Logger.e("Impossible to connect to Geocoder" + e.toString())
-        }
-
-        return null
-    }
-
-    private fun Context.getFirstAddress(): Address? {
-        val addresses = getGeoCoderAddress()
-        return if (addresses != null && addresses.isNotEmpty())
-            addresses[0]
-        else
-            null
-    }
-
-    fun Context.getAddressLine(): String = getFirstAddress()?.getAddressLine(0) ?: ""
-
-    fun Context.getLocality(): String = getFirstAddress()?.locality ?: ""
-
-    fun Context.getPostalCode(): String = getFirstAddress()?.postalCode ?: ""
-
-    fun Context.getCountryName(): String = getFirstAddress()?.countryName ?: ""
-
     private fun isBetterLocation(location: Location, currentBestLocation: Location?): Boolean {
-        if (currentBestLocation == null) {
-            // 이전에 저장한 것이 없다면 새로 사용
+        if (currentBestLocation == null)
             return true
-        }
 
         val timeDelta = location.time - currentBestLocation.time
-        val isSignificantlyNewer = timeDelta > TWO_MINUTES // 시간차 2분 이상?
-        val isSignificantlyOlder = timeDelta < -TWO_MINUTES // 아니면 더 오래되었는지
-        val isNewer = timeDelta > 0 // 신규 위치정보 파악
+        val isSignificantlyNewer = timeDelta > TWO_MINUTES
+        val isSignificantlyOlder = timeDelta < -TWO_MINUTES
+        val isNewer = timeDelta > 0
 
-        // 만일 2분이상 차이난다면 새로운거 사용 (유저가 움직이기 때문)
         if (isSignificantlyNewer) {
             return true
         } else if (isSignificantlyOlder) {
             return false
         }
 
-        // Check whether the new location fix is more or less accurate
         val accuracyDelta = (location.accuracy - currentBestLocation.accuracy).toInt()
-        val isLessAccurate = accuracyDelta > 0 // 기존거가 더 정확함
-        val isMoreAccurate = accuracyDelta < 0 // 신규가 더 정확함
-        val isSignificantlyLessAccurate = accuracyDelta > 200 // 200이상 심각하게 차이남
-        val isFromSameProvider = isSameProvider(location.provider, currentBestLocation.provider) // 같은 프로바이더인지
+        val isLessAccurate = accuracyDelta > 0
+        val isMoreAccurate = accuracyDelta < 0
+        val isSignificantlyLessAccurate = accuracyDelta > 200
+        val isFromSameProvider = isSameProvider(location.provider, currentBestLocation.provider)
 
-        if (isMoreAccurate) { // 더 정확하면?
+        if (isMoreAccurate) {
             return true
-        } else if (isNewer && !isLessAccurate) { // 새로운 데이터이고 신규가 정확하거나 같을때
+        } else if (isNewer && !isLessAccurate) {
             return true
-        } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) { // 새로운 데이터이고 너무 차이나지 않고 같은 프로바이더인 경우
+        } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
             return true
         }
         return false
@@ -185,16 +192,17 @@ class Locations : Service() {
 
     private fun isSameProvider(provider1: String?, provider2: String?): Boolean = if (provider1 == null) provider2 == null else provider1 == provider2
 
+    private fun checkIfLocationIsEnabled() :Boolean = (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+
     private inner class LocationChangeListener : android.location.LocationListener {
         override fun onLocationChanged(location: Location?) {
-            if (location == null) {
+            if (location == null)
                 return
-            }
 
             if (isBetterLocation(location, currentBestLocation)) {
                 currentBestLocation = location
                 currentBestLocation?.bearing = bearing
-                locationCallback?.handleNewLocation(currentBestLocation as Location)
+                locationCallback?.onSuccess(currentBestLocation as Location)
             }
         }
 
@@ -229,6 +237,16 @@ class Locations : Service() {
         }
     }
 
+    private inner class MyLocationListener : LocationListener {
+        override fun onLocationChanged(location :Location) {
+            if (isBetterLocation(location, currentBestLocation)) {
+                currentBestLocation = location
+                currentBestLocation?.bearing = bearing
+                locationCallback?.onSuccess(currentBestLocation as Location)
+            }
+        }
+    }
+
     private fun readDisplayRotation() {
         axisX = SensorManager.AXIS_X
         axisY = SensorManager.AXIS_Y
@@ -250,7 +268,37 @@ class Locations : Service() {
             get() = this@Locations
     }
 
+    //Address
+    private fun Context.getGeoCoderAddress(): List<Address>? {
+        val geoCoder = Geocoder(this, Locale.ENGLISH)
+        try {
+            return geoCoder.getFromLocation(currentBestLocation?.latitude ?: 0.0, currentBestLocation?.longitude ?: 0.0, 1)
+        } catch (e: IOException) {
+            Logger.e("Impossible to connect to Geocoder" + e.toString())
+        }
+
+        return null
+    }
+
+    private fun Context.getFirstAddress(): Address? {
+        val addresses = getGeoCoderAddress()
+        return if (addresses != null && addresses.isNotEmpty())
+            addresses[0]
+        else
+            null
+    }
+
+    fun Context.getAddressLine(): String = getFirstAddress()?.getAddressLine(0) ?: ""
+
+    fun Context.getLocality(): String = getFirstAddress()?.locality ?: ""
+
+    fun Context.getPostalCode(): String = getFirstAddress()?.postalCode ?: ""
+
+    fun Context.getCountryName(): String = getFirstAddress()?.countryName ?: ""
+
     interface LocationCallback {
-        fun handleNewLocation(location: Location)
+        fun locationDisabled()
+        fun onSuccess(location :Location)
+        fun onFailure(e :Exception)
     }
 }
--- kotlinextensions/src/main/kotlin/com/extensions/lifecycleobserver/MyLocationUpdateObserver.kt
+++ kotlinextensions/src/main/kotlin/com/extensions/lifecycleobserver/MyLocationUpdateObserver.kt
@@ -1,6 +1,5 @@
 package com.extensions.lifecycleobserver
 
-import android.annotation.SuppressLint
 import android.arch.lifecycle.Lifecycle
 import android.arch.lifecycle.LifecycleObserver
 import android.arch.lifecycle.OnLifecycleEvent
@@ -8,59 +7,19 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
-import android.location.Location
-import android.location.LocationManager
 import android.os.IBinder
-import com.extensions.content.locationManager
 import com.extensions.general.Locations
-import com.extensions.general.asDateString
-import java.util.*
-import android.os.Bundle
-import android.os.AsyncTask
-import com.extensions.threads.runInBackground
 
-class MyLocationUpdateObserver(var context: Context, var isFusedLocationApi: Boolean, var listener: MyLocationString) : LifecycleObserver {
+class MyLocationUpdateObserver(var context: Context, var isFusedLocationApi: Boolean, var listener: Locations.LocationCallback) : LifecycleObserver {
     lateinit var locationService:Locations
-    //var mGoogleApiClient :GoogleApiClient? = null
     var mBound = false
 
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     fun onResume() {
-        if(checkIfLocationIsEnabled()) {
-            if(isFusedLocationApi) {
-                /*runInBackground {
-                    if(mGoogleApiClient == null) {
-                        mGoogleApiClient = GoogleApiClient.Builder(context)
-                            .addConnectionCallbacks(object :GoogleApiClient.ConnectionCallbacks() {
-                                fun onConnected(bundle :Bundle) {
-                                    getLastLocation(context, locationListener)
-                                }
-
-                                fun onConnectionSuspended(i :Int) {
-                                }
-                            })
-                            .addOnConnectionFailedListener({connectionResult ->
-                                locationListener.onFailure(Exception(connectionResult.getErrorMessage()))
-
-                                if(mGoogleApiClient != null) {
-                                    if(mGoogleApiClient.isConnected())
-                                        mGoogleApiClient.disconnect()
-                                    mGoogleApiClient = null
-                                }
-                            })
-                            .addApi(LocationServices.API)
-                            .build()
-                    }
-                    if(!mGoogleApiClient.isConnected())
-                        mGoogleApiClient.connect()
-                }*/
-            } else {
-                val serviceIntent = Intent(context, Locations::class.java)
-                context.startService(serviceIntent)
-                context.bindService(serviceIntent, mConnection, Context.BIND_AUTO_CREATE)
-            }
-        } else
-            listener.locationDisabled()
+        val serviceIntent = Intent(context, Locations::class.java)
+        serviceIntent.putExtra(Locations.ARG_FUSED_LOCATION, isFusedLocationApi)
+        context.startService(serviceIntent)
+        context.bindService(serviceIntent, mConnection, Context.BIND_AUTO_CREATE)
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
@@ -71,10 +30,6 @@ class MyLocationUpdateObserver(var context: Context, var isFusedLocationApi: Boo
             mBound = false
         }
     }
-
-    private fun checkIfLocationIsEnabled() :Boolean =
-        (context.locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || context.locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
-
 
     private val mConnection = object : ServiceConnection {
         override fun onServiceConnected(className: ComponentName, service: IBinder) {
@@ -92,27 +47,6 @@ class MyLocationUpdateObserver(var context: Context, var isFusedLocationApi: Boo
     }
 
     private fun init() {
-        locationService.setLocationCallback({ location: Location ->
-            listener.getLocation(
-                    "${listener.setLocation()} \n Location changed! -> \n lat: ${location.latitude}\n " +
-                            "lng: ${location.longitude}\n provider: ${location.provider}\n " +
-                            "time: ${Calendar.getInstance().time.asDateString()}")
-        })
-
-        if (locationService.currentBestLocation != null) {
-            val location = locationService.currentBestLocation
-            listener.getLocation(
-                    "${listener.setLocation()}\n Location fetch! -> \n lat: ${location?.latitude}\n " +
-                            "lng: ${location?.longitude}\n provider: ${location?.provider}\n " +
-                            "time: ${Calendar.getInstance().time.asDateString()}")
-        } else {
-            listener.getLocation("${listener.setLocation()}\nNot fetched...")
-        }
-    }
-
-    interface MyLocationString {
-        fun getLocation(string: String)
-        fun setLocation(): String
-        fun locationDisabled()
+        locationService.setLocationCallback(listener)
     }
 }
